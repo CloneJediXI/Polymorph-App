aa49056d84880bb6372decb63b862d5a6555a103
Spencer Burke <sburke17@emich.edu>
2020-03-26 13:34:44 -0400

    Can Jump

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -6,10 +6,17 @@ public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D rb;//The rigid body on the player
     private GameObject overlord;//The State mananger fo the game
-    
+    private GameState state;
 
-    public float speed;
-    public float sprintModifier;
+    private int jumpCounter;
+    private int maxJumps = 1;//Number of extar jumps you get
+    private float jumpPower = 7;//How high you jump
+    private float groundCheckDistance = .1f;//How far to check for the ground
+    public LayerMask jumpCheckMask; //Set to what you want to be checked ie. the ground
+    private GameObject bottom;
+
+    private float speed = 7f;
+    private float sprintModifier = 4f;
 
     
 
@@ -17,21 +24,36 @@ public class PlayerMovement : MonoBehaviour
     {
         rb = this.GetComponent<Rigidbody2D>();
         overlord = GameObject.Find("Overlord");
+        state = overlord.GetComponent<GameState>();
+        bottom = GameObject.Find("Player/Bottom");
     }
 
-    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (grounded())
+        {
+            jumpCounter = 0;
+        }
+    }
     void Update()
     {
-
-        //Checks if moving left or right, no difference right now but might need it later
-        if (Input.GetAxisRaw("Horizontal") > 0.0f)
+        //If the game is not paused
+        if (!state.Paused)
         {
-            move(Input.GetButton("Fire3"), Input.GetAxisRaw("Horizontal"));
-        }else if (Input.GetAxisRaw("Horizontal") < 0.0f)
-        {
-            move(Input.GetButton("Fire3"), Input.GetAxisRaw("Horizontal"));
+            //Checks if moving left or right, no difference right now but might need it later
+            if (Input.GetAxisRaw("Horizontal") > 0.0f)
+            {
+                move(Input.GetButton("Fire3"), Input.GetAxisRaw("Horizontal"));
+            }else if (Input.GetAxisRaw("Horizontal") < 0.0f){
+                move(Input.GetButton("Fire3"), Input.GetAxisRaw("Horizontal"));
+            }
+            
+            if (Input.GetButtonDown("Jump"))
+            {
+                jump();
+            }
         }
-
+        
     }
     void move(bool sprint, float axisData)
     {
@@ -40,6 +62,25 @@ public class PlayerMovement : MonoBehaviour
         }else{
             rb.velocity = new Vector2(speed * axisData, rb.velocity.y);
         }
+    }
+    void jump()
+    {
+        if (jumpCounter < maxJumps)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpPower);
+            jumpCounter++;
+        }
+    }
+    bool grounded()
+    {
+        //If you are touching the ground
+        RaycastHit2D hit = Physics2D.Raycast(bottom.transform.position, -Vector2.up, groundCheckDistance, jumpCheckMask);
+        if (hit.collider != null)
+        {
+            return true;
+        }
+        else return false;
+        
     }
     
 }
