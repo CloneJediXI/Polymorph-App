e08b473fed59ce49225e05f8bd5359b744c4bc3d
krygielEmich <krygiel@emich.edu>
2020-03-28 14:02:59 -0400

    Added script for main camera along with following the player through the level: kyle

---
+++ Assets/Scripts/MainCamera.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCamera : MonoBehaviour
+{
+    public GameObject player;
+    public Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        //transform.position = new Vector3(player.potistion.x + offset.x,)
+        transform.position = player.transform.position + offset;
+    }
+}
