50780aa12418a7097e733da3b3a2edc63c9899eb
Spencer Burke <sburke17@emich.edu>
2020-03-30 15:35:44 -0400

    Can Change your size

--- Assets/Scripts/ShapeChange.cs
+++ Assets/Scripts/ShapeChange.cs
@@ -5,34 +5,41 @@ using UnityEngine;
 public class ShapeChange : MonoBehaviour
 {
 	
- public Behaviour active;
- public GameObject Block;
- private GameObject playerObj = null;
- public int range = 7;
- void Start()
- {
-	 active = (Behaviour)GetComponent("Halo");
-	 active.enabled = false;
-	 playerObj = GameObject.FindGameObjectWithTag("Player");
- }
- void OnMouseOver()
-    {
-	if(Vector3.Distance(Block.transform.position, playerObj.transform.position) < range)
-	{
-	  if(Input.GetKey("e"))
-	  {		  
-      active.enabled = true;
-	  }
-	  if(Input.GetKeyUp("e"))
-	  {
-		  print("TEST");
-	  }
-    }
+     public Behaviour active;
+     public GameObject Block;
+     private GameObject playerObj = null;
+     public int range = 7;
+     void Start()
+     {
+	     active = (Behaviour)GetComponent("Halo");
+	     active.enabled = false;
+	     playerObj = GameObject.FindGameObjectWithTag("Player");
+     }
+     void OnMouseOver(){
+        if(Vector3.Distance(Block.transform.position, playerObj.transform.position) < range)
+        {
+	        if(Input.GetKey("e"))
+	        {		  
+            active.enabled = true;
+	        }
+	        if(Input.GetKeyUp("e"))
+	        {
+		        print("TEST");
+	        }
+        }
 	}
 
     void OnMouseExit()
     {
       active.enabled = false;
+    }
+    void OnMouseDown()
+    {
+        if(active.enabled == true)
+        {
+            BlockSize temp = GetComponent<BlockSize>();
+            playerObj.GetComponent<PlayerSize>().changeSize(temp.width, temp.height);
+        }
     }
 	
 }
