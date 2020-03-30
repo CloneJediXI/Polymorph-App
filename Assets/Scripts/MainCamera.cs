c71a24341242b975ca74f20546d54e1bf1dff4ec
krygielEmich <krygiel@emich.edu>
2020-03-30 11:54:18 -0400

    Building level 1

--- Assets/Scripts/MainCamera.cs
+++ Assets/Scripts/MainCamera.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class MainCamera : MonoBehaviour
 {
     public GameObject player;
-    public Vector3 offset;
+    private Vector3 offset = new Vector3(0, 3.1f, -10);
 
     // Start is called before the first frame update
     void Start()
