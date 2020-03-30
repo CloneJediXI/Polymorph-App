bc1d9d837d35054ba7bf5ed7b2f8f4fa0124089c
krygielEmich <krygiel@emich.edu>
2020-03-30 14:18:35 -0400

    Re added main camera script

---
+++ Assets/Scripts/MainCamera.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCamera : MonoBehaviour {
+    public GameObject player;
+    private Vector3 offset = new Vector3(0, 3.1f, -10);
+
+    // Start is called before the first frame update
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        //transform.position = new Vector3(player.potistion.x + offset.x,)
+        transform.position = player.transform.position + offset;
+    }
+}
