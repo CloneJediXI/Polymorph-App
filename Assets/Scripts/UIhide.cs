3e038a92f8499e961e76c9fbc2d2ed3dc1944b09
derickso8 <60188207+derickso8@users.noreply.github.com>
2020-03-30 13:19:23 -0400

    Added script to hide UI with H

---
+++ Assets/Scripts/UIhide.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIhide : MonoBehaviour
+{
+	public GameObject UI;
+    void Update()
+    {
+        if(Input.GetKey("h"))
+		{
+			UI.SetActive(false);
+		}
+    }
+}
