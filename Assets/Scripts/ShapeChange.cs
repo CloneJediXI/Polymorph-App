85c06c82c85e119cf3f39f82cad563569cf7c28a
derickso8 <60188207+derickso8@users.noreply.github.com>
2020-03-30 10:47:56 -0400

    Added ShapeChange.cs
    Allows for glow effect to enable when block is moused over in a given range of the player

---
+++ Assets/Scripts/ShapeChange.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShapeChange : MonoBehaviour
+{
+	
+ public Behaviour active;
+ public GameObject Block;
+ private GameObject playerObj = null;
+ public int range = 7;
+ void Start()
+ {
+	 active = (Behaviour)GetComponent("Halo");
+	 active.enabled = false;
+	 playerObj = GameObject.FindGameObjectWithTag("Player");
+ }
+ void OnMouseOver()
+    {
+	if(Vector3.Distance(Block.transform.position, playerObj.transform.position) < range)
+	{
+      active.enabled = true;
+    }
+	}
+
+    void OnMouseExit()
+    {
+      active.enabled = false;
+    }
+}
