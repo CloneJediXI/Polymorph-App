bbd0413b2367830e5b395fb7ad84c7207b554ca8
Spencer Burke <sburke17@emich.edu>
2020-04-11 10:15:25 -0400

    Googley Eye

---
+++ Assets/Scripts/Player/Eyes.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Eyes : MonoBehaviour
+{
+    public Transform target;
+    public float speed = .9F;
+    
+
+    void FixedUpdate()
+    {
+        /*if((transform.position - target.position).magnitude > radius)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, target.position, (transform.position - target.position).magnitude - radius);
+        }*/
+        //float temp = Mathf.Abs((transform.position - target.position).magnitude) * speed;
+        transform.position = Vector3.Lerp(transform.position, target.position, speed);
+
+    }
+}
