d5eb5b3268085b19a89bd453fa253a82edefbb59
Spencer Burke <sburke17@emich.edu>
2020-04-14 10:37:50 -0400

    Fixed Wall Jump, Modified jump/Speed values

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -8,12 +9,12 @@ public class PlayerMovement : MonoBehaviour
 
     private int jumpCounter;
     private int maxJumps = 1;//Number of extar jumps you get
-    private float jumpPower = 6.5f;//How high you jump
+    private float jumpPower = 5.5f;//How high you jump
     private float groundCheckDistance = .1f;//How far to check for the ground
     public LayerMask jumpCheckMask; //Set to what you want to be checked ie. the ground
     public GameObject[] bottom;
 
-    private float speed = 7f;
+    private float speed = 6.5f;
     private float sprintModifier = 4f;
 
     private Animator anim;
@@ -31,20 +32,24 @@ public class PlayerMovement : MonoBehaviour
     private float eyeStart;
     private float eyeOfset = .1f;
 
+    
     public bool[] feet;
+    
     public bool inWallRight = false;
     public bool inWallLeft = false;
+    public bool onGround;
+    public bool tempOnground;
 
     void Start()
     {
         rb = this.GetComponent<Rigidbody2D>();
         overlord = GameObject.Find("Overlord");
         state = overlord.GetComponent<GameState>();
-        bottom = new GameObject[7];
-        feet = new bool[7];
-        for(int i = 0; i<7; i++)
+        bottom = new GameObject[5];
+        feet = new bool[5];
+        for(int i = 0; i<bottom.Length; i++)
         {
-            bottom[i] = transform.GetChild(i).gameObject;
+            bottom[i] = transform.GetChild(i+1).gameObject;
         }
         //bottom = new GameObject[] { GameObject.Find("Player/Bottom1"), GameObject.Find("Player/Bottom2"), GameObject.Find("Player/Bottom3") };
 
@@ -57,7 +62,8 @@ public class PlayerMovement : MonoBehaviour
 
     void FixedUpdate()
     {
-        if (grounded())
+        grounded();
+        if (onGround)
         {
             jumpCounter = 0;
         }
@@ -117,30 +123,27 @@ public class PlayerMovement : MonoBehaviour
             jumpCounter++;
         }
     }
-    bool grounded()
+    void grounded()
     {
-        //Reset all the variables
-        for(int i =0; i<feet.Length; i++)
-        {
-            feet[i] = false;
-        }
-        inWallLeft = false;
-        inWallRight = false;
 
 
         //If the two ends are in the wall
-        RaycastHit2D hit = Physics2D.Raycast(bottom[0].transform.position, Vector2.up, groundCheckDistance, jumpCheckMask);
+        /*RaycastHit2D hit = Physics2D.Raycast(bottom[0].transform.position, Vector2.up, groundCheckDistance, jumpCheckMask);
         if (hit.collider != null)
         {
             inWallLeft = true;
+        }
+        else
+        {
+            inWallLeft = false;
         }
         hit = Physics2D.Raycast(bottom[bottom.Length-1].transform.position, Vector2.up, groundCheckDistance, jumpCheckMask);
         if (hit.collider != null)
         {
             inWallRight = true;
-        }
+        }*/
 
-
+        RaycastHit2D hit;
         //Check all of the feet
         for (int i = 0; i < bottom.Length; i++)
         {
@@ -150,45 +153,60 @@ public class PlayerMovement : MonoBehaviour
             {
                 feet[i] = true;
             }
+            else
+            {
+                feet[i] = false;
+            }
         }
 
 
         //If it is in the wall, check to see if any of the others are on the ground
         //If so, then jump
-        if (inWallRight)
+        /*if (inWallRight)
         {
-            for(int i = 0; i<feet.Length-1; i++)
+            for(int i = 1; i<feet.Length; i++)
             {
                 if (feet[i])
                 {
                     return true;
-                }
-            }
-        }else if (inWallLeft)
-        {
-            for (int i = 1; i < feet.Length; i++)
-            {
-                if (feet[i])
-                {
-                    return true;
+
                 }
             }
         }
-
-        //If it is not in the wall, any of the feet will do
-        if(!inWallLeft && !inWallRight)
+        else if (inWallLeft)
         {
+            for (int i = 1; i < feet.Length-1; i++)
+            {
+                if (feet[i])
+                {
+                    return true;
+                }
+            }
+        }*/
+        tempOnground = false;
+        //If it is not in the wall, any of the feet will do
             for (int i = 0; i < feet.Length; i++)
             {
                 if (feet[i])
                 {
-                    return true;
+                tempOnground = true;
                 }
             }
+        if (tempOnground)
+        {
+            onGround = true;
         }
+        else
+        {
+            StartCoroutine(jumpWait());
+        }
+        
 
-        return false;
-
+    }
+    IEnumerator jumpWait()
+    {
+        yield return new WaitForSeconds(.2f);
+        onGround = false;
     }
     public float JumpPower{ get{return jumpPower;} set{jumpPower = value;} }
     public float Speed{ get{return speed;} set{speed = value;} }
